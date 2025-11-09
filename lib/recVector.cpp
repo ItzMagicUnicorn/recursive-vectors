71c87cb88c3bc154c12814461899dc3701157f0d
m0xn <moon0xcoder@gmail.com>
2025-11-09 13:44:18 +0100

    Fixed bugs, and implemented insert functions
    Fixed the implementation for basic functions and
    implemeneted helper functions to shift the elements
    of the vector. Implemented pushFront, pushBack and
    insert to modify the contents of the vector. Changed
    resetIndex for endProcessing to update v.count
    accordingly.

--- lib/recVector.cpp
+++ lib/recVector.cpp
@@ -7,26 +7,38 @@ using namespace std;
 bool seeded = false;
 
 // ------- Reset functions -------
-void resetIndex(RecVOV& v) {
+void endProcessing(RecVOV& v, int count) {
 	v.i = 0;
+	v.count = count;
 }
 
 // ------- Init functions -------
 void init(RecVOV& v) {
 	v.count = v.i = 0;
 }
-void initRand(RecVOV& v, int count, int max) {
+
+// ------- Fill functions -------
+void fill(RecVOV &v, int count, int val) {
+	if (v.i < count) {
+		v.vector[v.i] = val;
+		v.i++;
+		fill(v, count, val);
+	}
+	else endProcessing(v, count);
+}
+
+void fillRand(RecVOV& v, int count, int max) {
 	if (!seeded) {
 		srand(time(NULL));
 		seeded = true;
 	}
+
 	if (v.i < count) {
 		v.vector[v.i] = rand() % max + 1;
 		v.i++;
-		v.count++;
-		initRand(v, count, max);
+		fillRand(v, count, max);
 	}
-	else resetIndex(v);
+	else endProcessing(v, count);
 }
 
 // ------- Output functions -------
@@ -35,28 +47,60 @@ void show(RecVOV& v) {
 	if (v.i < v.count) {
 		cout << v.vector[v.i] << " ";
 		v.i++;
+		show(v);
 	}
 	else {
-		cout << "}";
-		resetIndex(v);
+		endProcessing(v, v.count);
+		cout << "}" << endl;
 	}
 }
 
+// ------- Shift functions (helper functions) -------
+void shiftRight(RecVOV &v, int untilPos) {
+	if (v.i < untilPos) {
+		v.vector[v.count - v.i] = v.vector[v.count - v.i - 1];
+		v.i++;
+		shiftRight(v, untilPos);
+	}
+	else endProcessing(v, v.count);
+}
+
+void shiftLeft(RecVOV &v, int untilPos) {
+	if (v.i < untilPos) {
+		v.vector[v.i] = v.vector[v.i + 1];
+		v.i++;
+		shiftLeft(v, untilPos);
+	} else endProcessing(v, v.count);
+}
+
 // ------- Insert functions -------
-void pushFront(RecVOV& v, int val);
-void pushBack(RecVOV& v, int val);
-void insert(RecVOV& v, int pos, int val);
+void pushFront(RecVOV& v, int val) {
+	shiftRight(v, v.count);
+	v.vector[0] = val;
+	endProcessing(v, v.count + 1);
+}
+
+void pushBack(RecVOV &v, int val) {
+	v.vector[v.count] = val;
+	v.count++;
+}
+
+void insert(RecVOV &v, int pos, int val) {
+	shiftRight(v, pos);
+	v.vector[pos] = val;
+	endProcessing(v, v.count + 1);
+}
 
 // ------- Erase functions -------
-void erase(RecVOV& v, int val);
-void eraseAll(RecVOV& v, int val);
-void eraseAt(RecVOV& v, int pos);
+void erase(RecVOV &v, int val);
+void eraseAt(RecVOV&v, int pos);
+void eraseAll(RecVOV&v, int val);
 
 // ------- Search functions -------
-int search(RecVOV& v, int val, int& pos);
+int search(RecVOV&v, int val, int& pos);
 
 // ------- Arithmetic functions -------
-int add(RecVOV& v, RecVOV& w, RecVOV& r);
-int substract(RecVOV& v, RecVOV& w, RecVOV& r);
-int mult(RecVOV& v, RecVOV& w, RecVOV& r);
-int divide(RecVOV& v, RecVOV& w, RecVOV& r);
+void add(RecVOV&v, RecVOV& w, RecVOV& r);
+void substract(RecVOV&v, RecVOV& w, RecVOV& r);
+void mult(RecVOV&v, RecVOV& w, RecVOV& r);
+void divide(RecVOV&v, RecVOV& w, RecVOV& r);
--- lib/recVector.h
+++ lib/recVector.h
@@ -10,17 +10,23 @@ struct RecVOV {
 	int i; //index
 };
 
-// ------- Reset functions -------
-void resetIndex(RecVOV &v);
+// ------- Update functions -------
+void endProcessing(RecVOV &v, int count);
 
 // ------- Init functions -------
 void init(RecVOV &v);
 
-//PRE:count>0
-void initRand(RecVOV &v, int count, int max);
+// ------- Fill functions -------
+void fill(RecVOV &v, int count, int val);
+void fillRand(RecVOV &v, int count, int max);
+// void fillKB(RecVOV &v, int count, int val);
 
 // ------- Output functions -------
 void show(RecVOV &v);
+
+// ------- Shift functions (helper functions) -------
+void shiftRight(RecVOV &v, int untilPos);
+void shiftLeft(RecVOV &v, int untilPos);
 
 // ------- Insert functions -------
 void pushFront(RecVOV &v, int val);
@@ -29,16 +35,16 @@ void insert(RecVOV &v, int pos, int val);
 
 // ------- Erase functions -------
 void erase(RecVOV &v, int val);
-void eraseAll(RecVOV &v, int val);
 void eraseAt(RecVOV &v, int pos);
+void eraseAll(RecVOV &v, int val);
 
 // ------- Search functions -------
 int search(RecVOV &v, int val, int &pos);
 
 // ------- Arithmetic functions -------
-int add(RecVOV &v, RecVOV &w, RecVOV &r);
-int substract(RecVOV &v, RecVOV &w, RecVOV &r);
-int mult(RecVOV &v, RecVOV &w, RecVOV &r);
-int divide(RecVOV &v, RecVOV &w, RecVOV &r);
+void add(RecVOV &v, RecVOV &w, RecVOV &r);
+void substract(RecVOV &v, RecVOV &w, RecVOV &r);
+void mult(RecVOV &v, RecVOV &w, RecVOV &r);
+void divide(RecVOV &v, RecVOV &w, RecVOV &r);
 
 #endif // _REC_VECTOR_H
